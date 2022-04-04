c2c008d664bfc927387043f3c5b5d73d04c5e7cb
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-04 15:15:55 +0100

    Add Trimmer Decorator class that inherits Decorator and trims the @nameable if it is more than 10 words with the correct_name method

--- decorator.rb
+++ decorator.rb
@@ -19,3 +19,9 @@ class CapitalizeDecorator < Decorator
     @nameable.correct_name.capitalize
   end
 end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.slice(0, 10) unless @nameable.correct_name.length <= 10
+  end
+end
