b5676dc17cb9c25c3f270d8f66bdac15cb4fb567
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-06 15:06:28 +0100

    Fix error when creating object with parent_permissions set to false

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(name, parent_permission, age)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
