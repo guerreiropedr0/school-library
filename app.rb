a10c7e08dda2839dddc3e57295a7339f463b3562
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-07 09:36:17 +0100

    Add method list_people in app.rb file

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ def list_books(books)
     puts "Title: #{book.title}, Author: #{book.author}"
   end
 end
+
+def list_people(people)
+  people.each do |person|
+    puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  end
+end
