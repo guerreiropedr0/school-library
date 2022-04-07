3ca2c54af85c3669a4f33c7a711656e5fa2a4902
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-07 14:02:44 +0100

    Changes in app.rb for methods, now they do the interactivity on the terminal instead on main.rb

--- app.rb
+++ app.rb
@@ -36,15 +36,40 @@ class App
     @books.push(Book.new(title, author))
   end
 
-  def create_student(age, name, parent_permission)
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    education = gets.chomp.to_i
+    case education
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      puts 'You can only choose between (1) or (2)'
+    end
+  end
+  
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
     @people.push(Student.new(age, nil, name, parent_permission: parent_permission))
   end
 
-  def create_teacher(age, name, specialization)
-    @people.push(Student.new(age, specialization, name))
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    @people.push(Teacher.new(age, specialization, name))
   end
 
-  # def create_rental(date, person, book)
-  #   @rentals.push(Rental.new(date, person, book))
-  # end
+  def create_rental(date, person, book)
+    @rentals.push(Rental.new(date, person, book))
+  end
 end
--- main.rb
+++ main.rb
@@ -3,21 +3,21 @@ require_relative './app'
 def menu
   puts
   puts 'Please choose an option by entering a number:'
-  
+
   options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
-    'List all rentals for a given person id', 'Exit']
-    
-    options.each_with_index do |option, index|
-      puts "#{index + 1} - #{option}"
-    end
+             'List all rentals for a given person id', 'Exit']
+
+  options.each_with_index do |option, index|
+    puts "#{index + 1} - #{option}"
   end
-  
-  def main
-    app = App.new
-    
-    puts 'Welcome to School Library App!'
-    while menu
-      selected = gets.chomp.to_i - 1
+end
+
+def main
+  app = App.new
+
+  puts 'Welcome to School Library App!'
+  while menu
+    selected = gets.chomp.to_i - 1
     case selected
     when 0
       app.list_books
@@ -25,25 +25,7 @@ def menu
       app.list_people
 
     when 2
-      print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-      education = gets.chomp.to_i
-      print 'Age: '
-      age = gets.chomp.to_i
-      print 'Name: '
-      name = gets.chomp
-
-      if education == 1
-        print 'Has parent permission? [Y/N]: '
-        parent_permission = gets.chomp
-        app.create_student(age, name, parent_permission)
-
-      else
-        print 'Specialization: '
-        specialization = gets.chomp
-        app.create_teacher(age, name, specialization)
-      end
-      puts 'Person created successfully'
-
+      app.create_person
     when 3
       print 'Title: '
       title = gets.chomp
@@ -52,6 +34,10 @@ def menu
       app.create_book(title, author)
       puts 'Book created successfully'
 
+    when 4
+      puts 'Select a book from the following list by number'
+      app.list_books
+      book = gets.chomp
     when 6
       break
     end
