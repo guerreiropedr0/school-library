0ee3d07534656f6ae943a024a83d2dffb46fb4ec
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-04-07 10:20:38 +0100

    Added App class and moved methods to class

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
-def list_books(books)
-  books.each do |book|
-    puts "Title: #{book.title}, Author: #{book.author}"
+class App
+  attr_accessor :books, :people
+  def initialize
+    @books = []
+    @people = []
   end
-end
+  
+  def list_books
+    @books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
 
-def list_people(people)
-  people.each do |person|
-    puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-  end
+  def list_people
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+  end  
 end
